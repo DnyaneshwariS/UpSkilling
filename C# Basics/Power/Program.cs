02608f09eacd07a236e5a7141c35df73e543efae
dnsuryav <dnsuryav@LIN13003746>
2023-05-08 15:10:06 +0530

    add

---
+++ C# Basics/Power/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Power
+{
+    class Power{
+ 
+    public static int power(int x, int n)
+    {
+ int result=1;
+      
+        if (n == 0)
+            return 1;
+ 
+
+        if (x == 0)
+            return 0;
+ 
+       for ( int i = 1; i <= n; i ++)
+
+                  result = result * x;
+                  return result;
+    }
+ 
+    public static void Main(String[] args)
+    {
+        int x = 2;
+        int n = 3;
+ 
+        Console.WriteLine(power(x, n));
+    }
+}
+}
---
+++ C# Basics/reverse string/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first string");
+        string a=Console.ReadLine();
+         Console.WriteLine("Enter the second string");
+        string b=Console.ReadLine();
+           string reversedString = null; 
+     for(int i = b.Length - 1; i >= 0; i--) 
+    { 
+         
+        reversedString += b[i]; 
+    }
+      if(a.Equals(reversedString))
+      {
+          Console.WriteLine("Both are reversed");
+      }
+      else
+      {
+                Console.WriteLine("Both are not reversed");
+      }
+        
+    }
+
+}
